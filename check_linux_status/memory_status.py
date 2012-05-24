7f66db8c4cf9da5db19846202c42521b49e8bcea
klobodnf <kaidxc@gmail.com>
2012-05-24 18:32:07 +0800

    modify the memory_status.py to normal.

--- check_linux_status/memory_status.py
+++ check_linux_status/memory_status.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import re
+
 re_meminfo_parser = re.compile(r'^(?P<key>\S*):\s*(?P<value>\d*)\s*kB')  
 
-def _get_mem_usage(self):  
+def _get_mem_usage():  
 	""" 
 	get mem used by percent 
 	self.result = falot 
@@ -19,5 +21,9 @@ def _get_mem_usage(self):
 			continue # skip lines that don't parse  
 		key, value=match.groups(['key', 'value'])  
 		result[key]=int(value)  
-	#print "mem :", 100*(result["MemTotal"]-result["MemFree"])/result["MemTotal"]  
+	#print "mem :", 100*(result["MemTotal"]-result["MemFree"])/result["MemTotal"]
+	print "MemTotal:" + str(result["MemTotal"]) + "\n"
+	print "MemFree:" + str(result["MemFree"]) + "\n"
 	return 100.0*(result["MemTotal"]-result["MemFree"])/result["MemTotal"]  
+
+print _get_mem_usage()
